7f25eb0a4d0087ae67e10aa4562ab60c2a28455e
WouterSwerts <swerts.wouter@gmail.com>
2021-07-30 10:38:40 +0200

    header aanmaken

---
+++ header.php
@@ -0,0 +1,12 @@
+<body>
+<div class="logo">
+    <div class="foto">
+        <img src="img/logo.png" alt="logo 't Zjirafke">
+    </div>
+    <div class="tekst"></div>
+</div>
+<div class="navBar">
+
+</div>
+</body>
+

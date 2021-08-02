b2a9a7847bec164f094268a56dcf02cf059cdbfb
WouterSwerts <swerts.wouter@gmail.com>
2021-08-02 15:23:53 +0200

    Dagje klaar

---
+++ dagje.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welkom bij kinderdagverblijf 't Zjirafke</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+    <!--    <link rel="manifest" href="img/site.webmanifest">-->
+    <link rel="mask-icon" href="img/safari-pinned-tab.svg" color="#77bdf2">
+    <meta name="msapplication-TileColor" content="#77bdf2">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="styles.css">
+    <link href='https://fonts.googleapis.com/css?family=Unkempt' rel='stylesheet'>
+    <link href="http://fonts.googleapis.com/css?family=Henny+Penny" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+<div class="hoofding">
+    <?php include "header.php"?>
+</div>
+
+<div class="dagjeGrid">
+    <div class="dagje tekstbox">
+        <h4>Een dagje in 't Zjirafke</h4>
+        <p>Onze kindjes zijn welkom vanaf 7u.</p>
+        <p>Wij vinden het belangrijk om regelmaat te bieden. Op die manier ontstaat er voor de kinderen een gevoel van herkenning, regelmaat en rust. Zo zijn er vaste momenten voor eten, slapen en verschonen (dit gebeurt natuurlijk steeds wanneer het nodig is). Tussendoor wordt er gespeeld, geknutseld, gaan we buiten spelen, dansen, zingen.</p>
+        <br>
+    </div>
+    <div class="contactDagje tekstbox">
+        <h4>Contacteer ons</h4>
+        <p>Hovenierstraat 2A - 2400 MOL-RAUW</p>
+        <p>Tel: 014/70.36.69</p>
+        <p>GSM Ria: 0498/11 04 79</p>
+        <p>GSM Katrien: 0497/28 96 39</p>
+        <p>Email: info@tzjirafke.be</p>
+        <a href="contact.php"><button>Lees meer</button></a>
+    </div>
+    <div class="dagindeling tekstbox">
+        <h4>Dagindeling</h4>
+        <ul>
+            <li>Vanaf 7u:
+            <ul>
+                <li>onthaal van de kindjes</li>
+                <li>tot 9u45 kunnen ze vrij spelen of onder begeleiding knutselen, kleuren, dansen, zingen,..., de baby’s kunnen ondertussen nog een dutje doen</li>
+            </ul>
+            </li>
+            <li>09u45:
+                <ul>
+                    <li>tijd om verse soep te eten en drinken we water</li>
+                </ul>
+            </li>
+            <li>10u00:
+                <ul>
+                    <li>verluieren en/of op het potje gaan</li>
+                </ul>
+            </li>
+            <li>11u00:
+                <ul>
+                    <li>samen opruimen en handjes wassen</li>
+                </ul>
+            </li>
+            <li>11u15:
+                <ul>
+                    <li>warme maaltijd</li>
+                </ul>
+            </li>
+            <li>11u45:
+                <ul>
+                    <li>Eventjes spelen en ondertussen klaarmaken om te gaan slapen</li>
+                </ul>
+            </li>
+            <li>12u15:
+                <ul>
+                    <li>tijd voor een middagdutje</li>
+                </ul>
+            </li>
+            <li>15u15:
+                <ul>
+                    <li>fruitpap voor de kleinste en boterhammetjes voor de grotere kinderen</li>
+                </ul>
+            </li>
+            <li>15u45:
+                <ul>
+                    <li>vrij of gericht spelen tot mama of papa komt</li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+    <div class="openingsurenVerlof tekstbox">
+        <h4>Openingsuren en verlof</h4>
+        <p>’t Zjirafke is elke werkdag open van 7u tot 18u.</p>
+        <p>Wij zijn jaarlijks 4 weken gesloten: 1 week met Kerst, 1 week met Pasen en 2 weken tijdens de zomervakantie.</p>
+        <p>Deze periodes worden jaarlijks herzien en zullen steeds tijdig meegedeeld worden.</p>
+    </div>
+    <div class="ritme tekstbox">
+        <p>Bij de dagindeling houden wij rekening met de behoefte van elk kind.</p>
+        <p>Voor de baby’s volgen wij zoveel mogelijk hetzelfde ritme als dat wat ze thuis gewend zijn.</p>
+    </div>
+</div>
+
+<div class="footerDagje">
+    <?php include "footer.php"?>
+</div>
+
+
+
+
+
+
+</body>
+</html>
+
+

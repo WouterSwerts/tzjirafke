e86914b42f37a7ccbab18822e9889940e8fbfc04
WouterSwerts <swerts.wouter@gmail.com>
2021-08-02 09:28:32 +0200

    index afgewerkt

--- index.php
+++ index.php
@@ -28,24 +28,24 @@
         <p>'t Zjirafke is een kinderdagverblijf te Mol-Rauw dat wordt uitgebaat door Ria en Katrien. Via deze website hopen zij u een overzicht te geven over de werking van hun kinderdagverblijf.</p>
             <p>Na het maken van een afspraak ben je welkom om een kijkje te komen nemen.</p>
             <p>'t Zjirafke is open van maandag tot en met vrijdag van 7u tot 18u.</p>
-        <button>Lees meer</button>
+        <a href="over.php"><button>Lees meer</button></a>
 
     </div>
     <div class="gedicht tekstbox">
         <h4>Gedicht van een ouder</h4>
         <p>Een van de ouders heeft voor ons een gedichtje geschreven. Wij waren hierdoor zo ontroerd, dat we het graag met jullie willen delen.</p>
         <p>Druk hieronder op de knop om het te lezen!</p>
-        <button>Lees meer</button>
+        <a href="gedicht.php"><button>Lees meer</button></a>
     </div>
     <div class="ditdat tekstbox">
         <h4>Ditjes & Datjes</h4>
         <p>Het menu 'Ditjes & Datjes' bevat vooral praktische informatie voor de ouders zoals: hoe inschrijven, informatie over het IKT-systeem, huishoudelijk reglement en dergelijke meer.</p>
-        <button>Lees meer</button>
+        <a href="ditdat.php"><button>Lees meer</button></a>
     </div>
     <div class="dagje tekstbox">
         <h4>Dagje 't Zjirafke</h4>
         <p>Bij 't Zjirafke zal er steeds worden getracht om elk kind de individuele aandacht te geven die het nodig heeft. Doch werd er een algemene dagplanning opgesteld die je hier kan bekijken. </p>
-        <button>Lees meer</button>
+        <a href="dagje.php"><button>Lees meer</button></a>
     </div>
     <div class="contact tekstbox">
         <h4>Contacteer ons</h4>
@@ -54,17 +54,11 @@
         <p>GSM Ria: 0498/11 04 79</p>
         <p>GSM Katrien: 0497/28 96 39</p>
         <p>Email: info@tzjirafke.be</p>
-        <button>Lees meer</button>
+        <a href="contact.php"><button>Lees meer</button></a>
     </div>
 </div>
 
-<footer>
-    <a href="index.php">Home</a><p> - </p>
-    <a href="index.php">Home</a><p> - </p>
-    <a href="index.php">Home</a><p> - </p>
-    <a href="index.php">Home</a><p> - </p>
-    <a href="index.php">Home</a>
-</footer>
+
 
 
 
